0bf291cfee3fbafedce7212e813e4cc3cfac0add
wpmarkuk <wpmarkuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-21 11:28:49 +0000

    Handle saving of the data on a per post type basis rather than all in one option in the ooptions table. Minor bug fixes.
    git-svn-id: https://plugins.svn.wordpress.org/wp-post-type-meta/trunk@1117441 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/meta-update.php
+++ functions/meta-update.php
@@ -6,42 +6,47 @@
 function wpptm_update_post_type_meta() {
 		
 	/* check we have some posted meta information */
-	if( isset( $_POST[ 'wpptm_update_metainfo' ] ) ) {
-	
-		/* get the current post type */
-		$post_type = $_GET[ 'post_type' ];
+	if( isset( $_POST[ 'wpptm_settings' ][ 'wpptm_update_metainfo' ] ) ) {
+		
+		/* check the nonce */
+		if( check_admin_referer( 'wpptm_nonce_action', 'wpptm_nonce_field' ) ) {
 			
-		/* loop through each setting added */
-		foreach( $_POST as $key => $value ) {
-			
-			/* if the key is either the post type or the save button */
-			if( $key == 'wpptm_post_type' || $key == 'wpptm_update_metainfo' )
-				continue;
+			/* get the current post type */
+			$post_type = $_GET[ 'post_type' ];
 			
 			/* get the current saved values from options */
-			$wpptm_options = get_option( 'wpptm_meta' );
-							
-			/* add our posted setting to the options array */
-			$wpptm_options[ $key ] = $value;
+			$wpptm_options = get_option( 'wpptm_' . $post_type );
 
-			/* update this setting */
-			update_option( 'wpptm_meta', $wpptm_options );
+			/* loop through each setting added */
+			foreach( $_POST[ 'wpptm_settings' ] as $key => $value ) {
+				
+				/* if the key is either the post type or the save button */
+				if( $key == 'wpptm_post_type' || $key == 'wpptm_update_metainfo' )
+					continue;
+						
+				/* add our posted setting to the options array */
+				$wpptm_options[ $key ] = $value;
+	
+				/* update this setting */
+				update_option( 'wpptm_' . $post_type, $wpptm_options );
+				
+			} // end loop through each setting
+	
+			/* redirect the user to meta admin page with added query vars */
+			wp_redirect(
+				add_query_arg(
+					array(
+						'post_type' => $post_type,
+						'page' => $post_type . '-metainfo',
+						'updated' => 'true',
+						'post_type' => $post_type
+					),
+					$wp_get_referer
+				)
+			);
+			exit;
 			
-		} // end loop through each setting
-
-		/* redirect the user to meta admin page with added query vars */
-		wp_redirect(
-			add_query_arg(
-				array(
-					'post_type' => $post_type,
-					'page' => $post_type . '-metainfo',
-					'updated' => 'true',
-					'post_type' => $post_type
-				),
-				$wp_get_referer
-			)
-		);
-		exit;
+		} // end if nonce matches
 
 	}
 
